9555ae47c3dc04b7f9ab6047fafe41893e104dbc
parth-ditanwala <81432711+parth-ditanwala@users.noreply.github.com>
2023-06-05 13:22:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
